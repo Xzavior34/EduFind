85e52227db71512eb799fceee1e33ab0810ae850
Xzavior34 <philipinem7@gmail.com>
2025-10-28 22:06:32 +0100

    Update signup.tsx

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '@/libs/firebase'
+import { auth } from '@/_libs/firebase'
 import { sendWelcomeEmail } from '@/utils/email'
 
 export default function Signup() {
