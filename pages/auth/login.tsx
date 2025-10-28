85069c8f653161df284ae213cbe47938615a81eb
Xzavior34 <philipinem7@gmail.com>
2025-10-28 22:26:16 +0100

    Update login.tsx

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { signInWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '@/_libs/firebase'
+import { auth } from '@/api/_libs/firebase'
 import { sendLoginNotification } from '@/utils/email'
 
 export default function Login() {
